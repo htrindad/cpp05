2c25c01db5f8dfc24048c8f8efd2ee131d2cc49e
Hugo Miguel Trindade Parada <htrindad@student.42lisboa.com>
2026-01-22 12:17:18 +0000

    done

--- ex03/Intern.h
+++ ex03/Intern.h
@@ -6,12 +6,15 @@
 /*   By: htrindad <htrindad@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/22 11:51:49 by htrindad          #+#    #+#             */
-/*   Updated: 2026/01/22 12:08:36 by htrindad         ###   ########.fr       */
+/*   Updated: 2026/01/22 12:11:35 by htrindad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 #include "AForm.h"
+#include "PresidentialPardonForm.h"
+#include "RobotomyRequestForm.h"
+#include "ShrubberyCreationForm.h"
 
 class AForm;
 
@@ -21,7 +24,7 @@ class Intern
 		Intern();
 		~Intern();
 		Intern(const Intern &ref);
-		Inter &operator=(const Intern &ref);
+		Intern &operator=(const Intern &ref);
 
 		// methods
 		AForm *makeForm(const std::string formName, const std::string formTarget);
--- ex03/main.cpp
+++ ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: htrindad <htrindad@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/13 11:21:59 by htrindad          #+#    #+#             */
-/*   Updated: 2026/01/22 12:07:48 by htrindad         ###   ########.fr       */
+/*   Updated: 2026/01/22 12:13:55 by htrindad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,9 +27,9 @@ int main()
 
 	try
 	{
-		robot = intern.makeForm("RobotomyRequestForm", "robotnik");
-		shrubbery = intern.makeForm("InternCreationForm", "tomfooelry");
-		president = intern.makeForm("PresidentialPardonForm", "Mr. Trump");
+		robot = (RobotomyRequestForm *)intern.makeForm("RobotomyRequestForm", "robotnik");
+		shrubbery = (ShrubberyCreationForm *)intern.makeForm("InternCreationForm", "tomfooelry");
+		president = (PresidentialPardonForm *)intern.makeForm("PresidentialPardonForm", "Mr. Trump"); // I had absolutely no idea I had to type cast in C++
 		nip = intern.makeForm("Nothing in particular", NULL);
 	}
 	catch (std::exception &e)
@@ -42,7 +42,5 @@ int main()
 		delete shrubbery;
 	if (president)
 		delete president;
-	if (nip)
-		delete nip;
 	return 0;
 }
